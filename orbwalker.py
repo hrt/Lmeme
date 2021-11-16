8e2604aff1b7e0ae4c3e4e89892cd7b373372ed6
hrt <inusha@outlook.com>
2021-11-16 23:14:01 +0000

    Issue: attack speed is capped at 90 if hail of blades or lethal tempo is active
    master

--- orbwalker.py
+++ orbwalker.py
@@ -3,6 +3,8 @@ import mouse
 import keyboard
 from world import find_game_time
 
+LETHAL_TEMPO = 'ASSETS/Perks/Styles/Precision/LethalTempo/LethalTempoEmpowered.lua'
+HAIL_OF_BLADES = 'ASSETS/Perks/Styles/Domination/HailOfBlades/HailOfBladesBuff.lua'
 
 class OrbWalker:
     def __init__(self, mem):
@@ -12,17 +14,26 @@ class OrbWalker:
         self.can_move_time = game_time
 
     @staticmethod
-    def get_attack_time(champion, base_attack_speed):
-        # todo: attack speed isn't always capped at 2.5
-        attack_speed = min(2.5, champion.attack_speed_multiplier * base_attack_speed)
+    def get_attack_time(champion, base_attack_speed, attack_speed_cap):
+        attack_speed = min(attack_speed_cap, champion.attack_speed_multiplier * base_attack_speed)
         return 1. / attack_speed
 
     @staticmethod
-    def get_windup_time(champion, base_attack_speed, windup):
-        return OrbWalker.get_attack_time(champion, base_attack_speed) * windup
+    def get_windup_time(champion, base_attack_speed, windup, attack_speed_cap):
+        return OrbWalker.get_attack_time(champion, base_attack_speed, attack_speed_cap) * windup
+
+    @staticmethod
+    def get_attack_speed_cap(champion, game_time):
+        uncapped = False
+        uncapped |= any([buff.end_time > game_time for buff in champion.buffs[LETHAL_TEMPO]])
+        uncapped |= any([buff.end_time > game_time for buff in champion.buffs[HAIL_OF_BLADES]])
+        if uncapped:
+            return 90.
+        return 2.5
 
     def walk(self, stats, champion, x, y, game_time):
         mouse.press(mouse.MIDDLE)
+        attack_speed_cap = OrbWalker.get_attack_speed_cap(champion, game_time)
         if x is not None and y is not None and self.can_attack_time < game_time:
             stored_x, stored_y = mouse.get_position()
             mouse.move(int(x), int(y))
@@ -31,8 +42,8 @@ class OrbWalker:
             game_time = find_game_time(self.mem)
             attack_speed = stats.get_attack_speed(champion.name)
             windup = stats.get_windup(champion.name)
-            self.can_attack_time = game_time + OrbWalker.get_attack_time(champion, attack_speed)
-            self.can_move_time = game_time + OrbWalker.get_windup_time(champion, attack_speed, windup)
+            self.can_attack_time = game_time + OrbWalker.get_attack_time(champion, attack_speed, attack_speed_cap)
+            self.can_move_time = game_time + OrbWalker.get_windup_time(champion, attack_speed, windup, attack_speed_cap)
             mouse.move(stored_x, stored_y)
         elif self.can_move_time < game_time:
             mouse.right_click()
